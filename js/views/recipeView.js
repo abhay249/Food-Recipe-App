c52b7218c2ec08d15d44444ee060b911b44ec93c
abhay249 <abhaygupta24092002@gmail.com>
2023-02-19 12:56:10 +0530

    modified commit

--- js/views/recipeView.js
+++ js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from "./View.js";
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from "../../images/icons.svg"; // Parcel 2
-import { Fraction } from "fractional";
+// import { Fraction } from "fractional";
 import { numberToFraction } from "../helpers.js";
 
 class RecipeView extends View {
